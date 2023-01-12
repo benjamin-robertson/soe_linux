920814c6442cdddbe060a0de9da73fec11dd41f1
ben <benrobertson9876@gmail.com>
2023-01-12 12:12:40 +1100

    Remove notify

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -7,15 +7,15 @@
 class soe_linux::sudo (
   Hash $config,
 ) {
-  notify{"soe_linux sudo has is ${config}":}
+  # notify{"soe_linux sudo has is ${config}":}
 
-  if defined(Class['platform_module']) {
-    $platform_sudo_rules = lookup('platform_module::sudo')
-    notify{"soe_linux lookup platform rules ${platform_sudo_rules}":}
-    $final_rules = $config + $platform_sudo_rules
-  } else {
-    $final_rules = $config
-  }
+  # if defined(Class['platform_module']) {
+  #   $platform_sudo_rules = lookup('platform_module::sudo')
+  #   notify{"soe_linux lookup platform rules ${platform_sudo_rules}":}
+  #   $final_rules = $config + $platform_sudo_rules
+  # } else {
+  #   $final_rules = $config
+  # }
 
   file { '/tmp/finalsudo.txt':
     content => $final_rules.convert_to(String),
